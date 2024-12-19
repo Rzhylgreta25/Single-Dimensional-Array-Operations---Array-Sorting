97c9f4c09305cf000b5395750b63a00931089d7e
Your Name <rzhyl05@gmail.com>
2024-12-19 14:42:22 +0800

    your commit message

--- sortArray.js
+++ sortArray.js
@@ -1,11 +1,23 @@
+function mergeArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+function sortNumbersDescending(numbers) {
+    return numbers.sort((a, b) => b - a);
+}
+
+function sortNamesAlphabetically(names) {
+    return names.sort();
+}
+
 let numbers = [24, 65, 21, 5, 9, 32, 42, 80, 57];
 let names = ["Zenvo", "Erica", "Jordie", "Alicia", "Redon"];
 
-let mergedArray = numbers.concat(names);
+let mergedArray = mergeArrays(numbers, names);
 console.log("Merged Array:", mergedArray);
 
-numbers.sort((a, b) => b - a);
-console.log("Sorted Numbers (in reverse):", numbers);
+let sortedNumbers = sortNumbersDescending(numbers);
+console.log("Sorted Numbers (in reverse):", sortedNumbers);
 
-names.sort();
-console.log("Sorted Names (alphabetically):", names);
+let sortedNames = sortNamesAlphabetically(names);
+console.log("Sorted Names (alphabetically):", sortedNames);
